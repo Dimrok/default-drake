0e4f51489c24a164940d05236a7a12e2cdbc13da
Antony MECHIN <antony.mechin@docker.com>
2018-07-09 11:33:24 +0200

    Add dumb deployment script.
    Signed-off-by: Antony MECHIN <antony.mechin@docker.com>

---
+++ default-drake.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PROJECT=$1
+DRAKE_REPOSITORY=${2:-infinit}
+
+# Clone the default drake template.
+git clone https://github.com/Dimrok/default-drake $PROJECT
+cd $PROJECT
+
+# Remove unwanted files.
+rm -rf .git README.md
+
+# Initialize the git repository and add drake.
+git init
+git submodule add https://github.com/$DRAKE_REPOSITORY/drake
+
+# Intialize the project itself.
+echo '# $PROJECT' > README.md
+git add *
+git commit -s -m "Initialize project."
+
+# Create a virtualenv and install drake dependencies.
+cd _build/linux64 && virtualenv -p python3 .venv && . .venv/bin/activate && pip install -r ../../drake/requirements.txt
